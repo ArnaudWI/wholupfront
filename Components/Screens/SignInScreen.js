6fa51749111873c3259497e4951e5950309c461b
Arnaud Wissler <arnaud.wissler@gmail.com>
2019-03-05 15:34:45 +0100

    liaison avec le back pour l'ajout et l'identification

--- Components/Screens/SignInScreen.js
+++ Components/Screens/SignInScreen.js
@@ -9,6 +9,25 @@ import {
 } from 'react-native-elements'
 
 export default class SignInScreen extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  handleSubmit = (text) => {
+    fetch(`http://192.168.0.19:3000/signin?email=${this.state.email}&password=${this.state.password}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      if (data.isUserExist) {
+        this.setState({error: null});
+        this.props.navigation.navigate('Account');
+      } else {
+        this.setState({error: "l'identifiant ou/et le mot de passe sont incorrects"});
+      }
+    }).catch(error => console.error(error));
+  };
+
   render() {
     return (
       <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
@@ -16,18 +35,21 @@ export default class SignInScreen extends React.Component {
         <Input
           label="Email"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={text => this.setState({email: text})}
         />
         <Divider style={{height:10}}/>
         <Input
           label="Mot de passe"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={text => this.setState({password: text})}
+          errorMessage={this.state.error}
         />
         <Divider style={{height:20}}/>
         <Button
           title="Sign In"
           style={{width:100}}
           backgroundColor='#3498db'
-          onPress={ ()=> this.props.navigation.navigate('Account')}
+          onPress={this.handleSubmit}
         />
       </View>
     );
--- Components/Screens/SignUpScreen.js
+++ Components/Screens/SignUpScreen.js
@@ -9,6 +9,32 @@ import {
 } from 'react-native-elements'
 
 export default class SignUpScreen extends React.Component {
+
+state = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: ''
+};
+
+handleSubmit = () => {
+  fetch('http://192.168.0.19:3000/signup', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: 'firstName='+this.state.firstName+'&lastName='+this.state.lastName+'&email='+this.state.email+'&password='+this.state.password
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.user) {
+      this.props.navigation.navigate('Account');
+    } else {
+      console.log('erreur de log')
+    }
+  })
+  .catch(error => console.error(error))
+};
+
+
   render() {
     return (
       <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
@@ -16,28 +42,32 @@ export default class SignUpScreen extends React.Component {
         <Input
           label="First Name"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={(text) => this.setState({firstName: text})}
         />
         <Divider style={{height:10}}/>
         <Input
           label="Last Name"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={(text) => this.setState({lastName: text})}
         />
         <Divider style={{height:10}}/>
         <Input
           label="Email"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={(text) => this.setState({email: text})}
         />
         <Divider style={{height:10}}/>
         <Input
           label="Mot de passe"
           labelStyle={{textAlign: 'center'}}
+          onChangeText={(text) => this.setState({password: text})}
         />
         <Divider style={{height:20}}/>
         <Button
           title="Sign Up"
           style={{width:100}}
           backgroundColor='#3498db'
-          onPress={ ()=> this.props.navigation.navigate('Account')}
+          onPress={this.handleSubmit}
         />
       </View>
     );
